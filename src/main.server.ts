426148934fb42a8e8e6fba73a09e77eb5fa6542e
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-10-16 14:18:55 +0530

    angular material and its imports

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(AppComponent, {
+  ...config,
+  providers: [
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule
+  ]
+});
 
 export default bootstrap;
