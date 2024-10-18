737d4a67d221e46d4e41844b1ec640722968281b
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-10-18 12:05:52 +0530

    moviecard comp

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,26 @@
+// src/app/movie-card/movie-card.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss']
+})
+export class MovieCardComponent {
+  movies: any[] = []; //Array that stores the movies data.
+  constructor(public fetchApiData: FetchApiDataService) { }
+
+ngOnInit(): void { //this only runs after the component is initialized. It calls the getMovies method.
+  this.getMovies();
+}
+
+getMovies(): void {
+  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+}
